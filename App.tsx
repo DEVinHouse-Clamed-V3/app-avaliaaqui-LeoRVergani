d21d661304e0ae0ddd23fd3241a7e73b86586df4
leorvergani <vergani@outlook.com.br>
2024-10-08 01:25:06 -0300

    update

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
